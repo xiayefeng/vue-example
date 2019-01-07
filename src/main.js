ac9fa74a4d8c2b8b2d62f60712007bd5bd543dce
xiayefeng <xiayu_12@yeah.net>
2019-01-07 10:37:39 +0800

    update packge

--- src/main.js
+++ src/main.js
@@ -9,31 +9,31 @@ import './style/element-variables.scss'
 import regDirective from './directive/index'
 import regComponent from './directive/regComponent'
 
-import {isProd} from './api/util' // 是否生产环境
+import { isProd } from './api/util' // 是否生产环境
 
 Vue.config.productionTip = false
 if (isProd) {
-  Vue.config.devtools = false
-  Vue.config.silent = true
+    Vue.config.devtools = false
+    Vue.config.silent = true
 }
 
 regDirective()
 regComponent()
 
-Vue.config.errorHandler = function (err, vm, info) {
-  // handle error
-  // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
-  // 只在 2.2.0+ 可用
-  console.log(err)
-  console.log(info)
+Vue.config.errorHandler = function(err, vm, info) {
+    // handle error
+    // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
+    // 只在 2.2.0+ 可用
+    console.log(err)
+    console.log(info)
 }
 
 Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
